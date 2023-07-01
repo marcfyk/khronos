69babe62ffeefe888c61b83c7a0825517bd779cb
marcfyk <marcjohnfong@gmail.com>
2023-07-01 19:36:49 +0800

    Restructure options functions

--- app/CLI.hs
+++ app/CLI.hs
@@ -32,6 +32,7 @@ newtype Opts = Opts {optCommand :: Command}
 data Command
   = Now (Maybe Format)
   | Elapse (Maybe Format) Offset
+  | Range (Maybe Format) Take TimeStamp
 
 data Format = UNIX | ISO8601
 
@@ -42,12 +43,19 @@ data Offset = Offset
     seconds :: Maybe Integer
   }
 
+newtype Take = Take Integer
+
+newtype TimeStamp = TimeStamp Integer
+
+newtype Step = Step Integer
+
 run :: IO ()
 run = do
   opts <- execParser optsParser
   case optCommand opts of
     Now format -> execNow format
     Elapse format offset -> execElapse format offset
+    Range format take step -> undefined
   where
     optsParser :: ParserInfo Opts
     optsParser =
@@ -65,99 +73,98 @@ run = do
           ( nowCommand
               <> elapseCommand
           )
+    formatOptions :: Parser (Maybe Format)
+    formatOptions = optional $ unixFormat <|> isoFormat
       where
-        formatOptions :: Parser (Maybe Format)
-        formatOptions = optional $ unixFormat <|> isoFormat
-          where
-            unixFormat :: Parser Format
-            unixFormat =
-              flag'
-                UNIX
-                (long "unix" <> help "unix format")
+        unixFormat :: Parser Format
+        unixFormat =
+          flag'
+            UNIX
+            (long "unix" <> help "unix format")
 
-            isoFormat :: Parser Format
-            isoFormat =
-              flag'
-                ISO8601
-                ( long "iso8601"
-                    <> long "iso"
-                    <> help "ISO 8601 format"
-                )
-
-        nowCommand :: Mod CommandFields Command
-        nowCommand =
-          command
-            "now"
-            ( info
-                (nowOptions <**> helper)
-                (progDesc "Prints the current time in the specified format (default format is unix)")
+        isoFormat :: Parser Format
+        isoFormat =
+          flag'
+            ISO8601
+            ( long "iso8601"
+                <> long "iso"
+                <> help "ISO 8601 format"
             )
-          where
-            nowOptions :: Parser Command
-            nowOptions = Now <$> formatOptions
 
-        elapseCommand :: Mod CommandFields Command
-        elapseCommand =
-          command
-            "elapse"
-            ( info
-                (elapseOptions <**> helper)
-                (progDesc "Prints the elapse time from a given offset in the specified format (default format is unix)")
+    offsetOptions :: Parser Offset
+    offsetOptions =
+      Offset
+        <$> optional dayOffset
+        <*> optional hourOffset
+        <*> optional minuteOffset
+        <*> optional secondOffset
+      where
+        dayOffset :: Parser Integer
+        dayOffset =
+          option
+            auto
+            ( long "day"
+                <> long "dy"
+                <> short 'd'
+                <> metavar "DAY_OFFSET"
+                <> help "number of days relative to the current time"
             )
-          where
-            elapseOptions :: Parser Command
-            elapseOptions = Elapse <$> formatOptions <*> offsetOptions
 
-            offsetOptions :: Parser Offset
-            offsetOptions =
-              Offset
-                <$> optional dayOffset
-                <*> optional hourOffset
-                <*> optional minuteOffset
-                <*> optional secondOffset
+        hourOffset :: Parser Integer
+        hourOffset =
+          option
+            auto
+            ( long "hour"
+                <> long "hr"
+                <> short 'h'
+                <> metavar "HOUR_OFFSET"
+                <> help "number of hours relative to the current time"
+            )
+        minuteOffset :: Parser Integer
+        minuteOffset =
+          option
+            auto
+            ( long "minute"
+                <> long "min"
+                <> short 'm'
+                <> metavar "MINUTE_OFFSET"
+                <> help "number of minutes relative to the current time"
+            )
 
-            dayOffset :: Parser Integer
-            dayOffset =
-              option
-                auto
-                ( long "day"
-                    <> long "dy"
-                    <> short 'd'
-                    <> metavar "DAY_OFFSET"
-                    <> help "number of days relative to the current time"
-                )
+        secondOffset :: Parser Integer
+        secondOffset =
+          option
+            auto
+            ( long "second"
+                <> long "sec"
+                <> short 's'
+                <> metavar "SECOND_OFFSET"
+                <> help "number of seconds relative to the current time"
+            )
 
-            hourOffset :: Parser Integer
-            hourOffset =
-              option
-                auto
-                ( long "hour"
-                    <> long "hr"
-                    <> short 'h'
-                    <> metavar "HOUR_OFFSET"
-                    <> help "number of hours relative to the current time"
-                )
-            minuteOffset :: Parser Integer
-            minuteOffset =
-              option
-                auto
-                ( long "minute"
-                    <> long "min"
-                    <> short 'm'
-                    <> metavar "MINUTE_OFFSET"
-                    <> help "number of minutes relative to the current time"
-                )
+    nowCommand :: Mod CommandFields Command
+    nowCommand =
+      command
+        "now"
+        ( info
+            (nowOptions <**> helper)
+            (progDesc "Prints the current time in the specified format (default format is unix)")
+        )
+      where
+        nowOptions :: Parser Command
+        nowOptions = Now <$> formatOptions
 
-            secondOffset :: Parser Integer
-            secondOffset =
-              option
-                auto
-                ( long "second"
-                    <> long "sec"
-                    <> short 's'
-                    <> metavar "SECOND_OFFSET"
-                    <> help "number of seconds relative to the current time"
-                )
+    elapseCommand :: Mod CommandFields Command
+    elapseCommand =
+      command
+        "elapse"
+        ( info
+            (elapseOptions <**> helper)
+            (progDesc "Prints the elapse time from a given offset in the specified format (default format is unix)")
+        )
+      where
+        elapseOptions :: Parser Command
+        elapseOptions = Elapse <$> formatOptions <*> offsetOptions
 
     formatUnixSeconds :: Maybe Format -> UNIX.UNIXTime -> String
     formatUnixSeconds Nothing = UNIX.toUNIXString
