d0cfcd3741183a186bd782ad79865e4fa9716734
marcfyk <marcjohnfong@gmail.com>
2023-07-01 19:49:10 +0800

    Move options parsers

--- app/CLI.hs
+++ app/CLI.hs
@@ -57,6 +57,14 @@ run = do
     Elapse format offset -> execElapse format offset
     Range format take step -> undefined
   where
+    commandOptions :: Parser Opts
+    commandOptions =
+      Opts
+        <$> subparser
+          ( nowCommand
+              <> elapseCommand
+          )
+
     optsParser :: ParserInfo Opts
     optsParser =
       info
@@ -65,82 +73,6 @@ run = do
             <> progDesc "khronos provides commands to display the result of time operations"
             <> header "khronos - A CLI for time"
         )
-
-    commandOptions :: Parser Opts
-    commandOptions =
-      Opts
-        <$> subparser
-          ( nowCommand
-              <> elapseCommand
-          )
-    formatOptions :: Parser (Maybe Format)
-    formatOptions = optional $ unixFormat <|> isoFormat
-      where
-        unixFormat :: Parser Format
-        unixFormat =
-          flag'
-            UNIX
-            (long "unix" <> help "unix format")
-
-        isoFormat :: Parser Format
-        isoFormat =
-          flag'
-            ISO8601
-            ( long "iso8601"
-                <> long "iso"
-                <> help "ISO 8601 format"
-            )
-
-    offsetOptions :: Parser Offset
-    offsetOptions =
-      Offset
-        <$> optional dayOffset
-        <*> optional hourOffset
-        <*> optional minuteOffset
-        <*> optional secondOffset
-      where
-        dayOffset :: Parser Integer
-        dayOffset =
-          option
-            auto
-            ( long "day"
-                <> long "dy"
-                <> short 'd'
-                <> metavar "DAY_OFFSET"
-                <> help "number of days relative to the current time"
-            )
-
-        hourOffset :: Parser Integer
-        hourOffset =
-          option
-            auto
-            ( long "hour"
-                <> long "hr"
-                <> short 'h'
-                <> metavar "HOUR_OFFSET"
-                <> help "number of hours relative to the current time"
-            )
-        minuteOffset :: Parser Integer
-        minuteOffset =
-          option
-            auto
-            ( long "minute"
-                <> long "min"
-                <> short 'm'
-                <> metavar "MINUTE_OFFSET"
-                <> help "number of minutes relative to the current time"
-            )
-
-        secondOffset :: Parser Integer
-        secondOffset =
-          option
-            auto
-            ( long "second"
-                <> long "sec"
-                <> short 's'
-                <> metavar "SECOND_OFFSET"
-                <> help "number of seconds relative to the current time"
-            )
 
     nowCommand :: Mod CommandFields Command
     nowCommand =
@@ -166,6 +98,14 @@ run = do
         elapseOptions :: Parser Command
         elapseOptions = Elapse <$> formatOptions <*> offsetOptions
 
+    execNow :: Maybe Format -> IO ()
+    execNow format = UNIX.now >>= putStrLn . formatUnixSeconds format
+
+    execElapse :: Maybe Format -> Offset -> IO ()
+    execElapse format offset = result >>= putStrLn . formatUnixSeconds format
+      where
+        result = UNIX.elapse . offsetToSeconds $ offset
+
     formatUnixSeconds :: Maybe Format -> UNIX.UNIXTime -> String
     formatUnixSeconds Nothing = UNIX.toUNIXString
     formatUnixSeconds (Just UNIX) = UNIX.toUNIXString
@@ -184,10 +124,71 @@ run = do
         offsetValues = map (uncurry (*)) offsetPairs
         totalOffset = realToFrac . sum $ offsetValues
 
-    execNow :: Maybe Format -> IO ()
-    execNow format = UNIX.now >>= putStrLn . formatUnixSeconds format
+formatOptions :: Parser (Maybe Format)
+formatOptions = optional $ unixFormat <|> isoFormat
+  where
+    unixFormat :: Parser Format
+    unixFormat =
+      flag'
+        UNIX
+        (long "unix" <> help "unix format")
 
-    execElapse :: Maybe Format -> Offset -> IO ()
-    execElapse format offset = result >>= putStrLn . formatUnixSeconds format
-      where
-        result = UNIX.elapse . offsetToSeconds $ offset
+    isoFormat :: Parser Format
+    isoFormat =
+      flag'
+        ISO8601
+        ( long "iso8601"
+            <> long "iso"
+            <> help "ISO 8601 format"
+        )
+
+offsetOptions :: Parser Offset
+offsetOptions =
+  Offset
+    <$> optional dayOffset
+    <*> optional hourOffset
+    <*> optional minuteOffset
+    <*> optional secondOffset
+  where
+    dayOffset :: Parser Integer
+    dayOffset =
+      option
+        auto
+        ( long "day"
+            <> long "dy"
+            <> short 'd'
+            <> metavar "DAY_OFFSET"
+            <> help "number of days relative to the current time"
+        )
+
+    hourOffset :: Parser Integer
+    hourOffset =
+      option
+        auto
+        ( long "hour"
+            <> long "hr"
+            <> short 'h'
+            <> metavar "HOUR_OFFSET"
+            <> help "number of hours relative to the current time"
+        )
+    minuteOffset :: Parser Integer
+    minuteOffset =
+      option
+        auto
+        ( long "minute"
+            <> long "min"
+            <> short 'm'
+            <> metavar "MINUTE_OFFSET"
+            <> help "number of minutes relative to the current time"
+        )
+
+    secondOffset :: Parser Integer
+    secondOffset =
+      option
+        auto
+        ( long "second"
+            <> long "sec"
+            <> short 's'
+            <> metavar "SECOND_OFFSET"
+            <> help "number of seconds relative to the current time"
+        )
